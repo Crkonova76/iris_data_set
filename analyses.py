ac20a13b61cc27f537a775ab25ddf0b0cca94e29
Your Name <you@example.com>
2019-04-09 23:10:43 +0100

    initial push

---
+++ analyses.py
@@ -0,0 +1,11 @@
+# Martina Crkonova, April 2019
+# GMIT, Project Iris Data Set
+
+import pandas as pd
+data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+
+# Basic statistics
+print(data.describe())
+
+print(data.corr())
+
