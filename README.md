c4d0dab6bc6c826350818666f1b9262bb8b94aac
Your Name <you@example.com>
2019-04-14 19:33:00 +0100

    boxplot

--- README.md
+++ README.md
@@ -9,19 +9,19 @@ Four features were measured from each sample: the length and the width of the se
 
 ## Libraries
 
-Pandas - a data processing and CSV file I/O library
-Seaborn- a Python graphing library
-Matplotlib.pyplot
-Numpy
+* Pandas - a data processing and CSV file I/O library
+* Seaborn- a Python graphing library
+* Matplotlib- a Python 2D plotting library
+* Numpy-a fundamental package for scientific computing with Python
 
 ## Statistics methods used:
 
 ### *Mean*
 * The "average" number, found by adding all data points and dividing by by the number of data points
-..reference: https://www.khanacademy.org/math/statistics-probability/summarizing-quantitative-data/mean-median-basics/a/mean-median-and-mode-review
+>reference: https://www.khanacademy.org/math/statistics-probability/>summarizing-quantitative-data/mean-median-basics/a/mean-median-and-mode-review
 
 ### *Standard Deviation*
 * Quantifies the amount of variation or dispersion of a set of data values.A low standard deviation indicates that the data points tend to be close to the mean (also called the expected value) of the set, while a high standard deviation indicates that the data points are spread out over a wider range of values.
-..reference: https://en.wikipedia.org/wiki/Standard_deviation
+>reference: https://en.wikipedia.org/wiki/Standard_deviation
 
 ### *Minimum , Maximum , Range*
--- analyses.py
+++ analyses.py
@@ -1,4 +1,4 @@
-, April 2019
+# April 2019
 # GMIT, Project Iris Data Set
 
 # Imported libraries pandas, numpy, seaborn and matplot
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load Iris data set from provided url link
-data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data'
-                          '-fa14/gh-pages/data/iris.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+
 
 # Displaying the the first 5 lines of the data set
 print(data.head(5))
@@ -22,19 +22,39 @@ print(data.describe())
 # grouped by the species
 
 grouped = data.groupby('species')
-#grouped.agg([np.mean, np.std])
 print(grouped.agg([np.mean, np.std, np.min, np.max]))
+
+'''
+# Analyses of Variables-sepal length
+# Sepal lenght histogram on the basis of species
+#
+# data.plot.hist(by = 'petal_length', bins=50)
+# plt.show()
+'''
 
 
 #Analysing sepal length and width using scaterplot
-sns.FacetGrid(data, hue="species", size=5).map(
-    plt.scatter, "sepal_length", "sepal_width").add_legend()
+#Purpose- to identify the type of relationship between sepal lenght and width
+sns.FacetGrid(data, hue="species", height=5).map(plt.scatter, "sepal_length", "sepal_width").add_legend()
 plt.show()
 
 #Analysing petal length and width using scaterplot
-sns.FacetGrid(data, hue="species", size=5).map(
-    plt.scatter, "petal_length", "petal_width").add_legend()
+#Purpose- to identify the type of relationship between petal lenght and width
+sns.FacetGrid(data, hue="species", height=5).map(plt.scatter, "petal_length", "petal_width").add_legend()
 plt.show()
 
+#corelation between sepal/petal length and width based on species
 
+#plot showing the correlation between species
+sns.pairplot(data, hue='species')
+plt.show()
+
+#Boxplot used to estimate the distribution of values for each attribute
+boxplot = data.boxplot (column=['sepal_length','sepal_width','petal_length','petal_width'])
+plt.show()
+
+#Boxplot -distribution of the values considering each species
+
+boxplot = data.boxplot(column=['sepal_length','sepal_width','petal_length','petal_width'],by = 'species')
+plt.show()
 
